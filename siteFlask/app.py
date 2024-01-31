8f92df3c59d5d55e26d04e86319b877e0a24e417
arc-17 <ti.rarcanjo@gmail.com>
2024-01-30 21:17:12 -0300

    Add for

--- siteFlask/app.py
+++ siteFlask/app.py
@@ -4,6 +4,6 @@ app = Flask("projeto")
 
 @app.route("/")
 def ola_mundo():
-    return render_template("index.html"), 200
+    return render_template("index.html", meu_nome="ARC-17"), 200
 
 app.run()
