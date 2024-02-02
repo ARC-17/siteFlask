aabed2c0dbd1b9038cd5daba553e41d3bff5ef54
arc-17 <ti.rarcanjo@gmail.com>
2024-02-02 20:38:29 -0300

    Add get and post

--- siteFlask/app.py
+++ siteFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("projeto")
 
@@ -6,6 +6,19 @@ app = Flask("projeto")
 def ola_mundo():
     return render_template("index.html", meu_nome="ARC-17"), 200
 
+@app.route("/tipoget")
+def ir_get():
+    return render_template("get.html"), 200
+
+@app.route("/receber/", methods=['GET','POST'])
+def receber():
+    if request.method == 'GET':
+        return "Estou no tipo GET!", 200
+    elif request.method == "POST":
+        return "Estou no tipo POST!", 200
+@app.route("/tipopost")
+def ir_post():
+    return render_template("post.html"), 200
 @app.route("/informacao/")
 @app.route("/informacao/<nome>")
 @app.route("/informacao/<nome>/<idade>")
